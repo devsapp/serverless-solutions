fabd073b6527896ef0a6bc5062be5e80c5fc7f64
千风 <qianfeng.zqf@alibaba-inc.com>
2022-07-12 16:55:15 +0800

    add tablestore sink connector

---
+++ serverless-tablestore-sink-connector/readme.md
@@ -0,0 +1,194 @@
+# FcTableStoreSinkConnector 帮助文档
+
+<p align="center" class="flex justify-center">
+    <a href="https://www.serverless-devs.com" class="ml-1">
+    <img src="http://editor.devsapp.cn/icon?package=FCTablestoreSinkConnector&type=packageType">
+  </a>
+  <a href="http://www.devsapp.cn/details.html?name=FCTablestoreSinkConnector" class="ml-1">
+    <img src="http://editor.devsapp.cn/icon?package=FCTablestoreSinkConnector&type=packageVersion">
+  </a>
+  <a href="http://www.devsapp.cn/details.html?name=FCTablestoreSinkConnector" class="ml-1">
+    <img src="http://editor.devsapp.cn/icon?package=FCTablestoreSinkConnector&type=packageDownload">
+  </a>
+</p>
+
+<description>
+
+> ***fc tablestore sink connector***
+
+</description>
+
+## 应用简介
+本应用可以将您的原始输入数据进过预处理之后，写入到您创建应用时填写的 Tablestore 数据表中，支持批量传输及单条数据传输，传输的数据格式支持 cloudEvent Shema 以及自定义格式。 
+
+本应用在事件数据传输中主要负责数据的数据预处理以及数据投递，其流程可参考下图：
+
+![简介图](https://img.alicdn.com/imgextra/i1/O1CN01E9a3ZD230u3yoFO84_!!6000000007194-2-tps-2864-1082.png)
+
+Poller Service 从源端拉取数据后，再推送给本应用对应的 Sink Service，最终投递到下游服务中，Sink Service 中包含两个功能函数：
+- Transform Function： 数据预处理函数，可自定义预处理逻辑，处理后的源端数据会被发送到 Sink Function 中。
+- Sink Function：数据投递函数，接收数据后将其投递到下游服务中。
+
+如果您需要对数据进行转换，可以编写应用创建后的 transform 函数。否则您只需调用 sink 函数即可。
+
+
+## 前期准备
+使用该项目，推荐您拥有以下的产品权限 / 策略：
+
+| 服务/业务 | 函数计算 |     
+| --- |  --- |   
+| 权限/策略 | AliyunFCFullAccess<br>AliyunLogFullAccess<br>AliyunOTSFullAccess |     
+
+<codepre id="codepre">
+
+
+
+</codepre>
+
+<deploy>
+
+## 部署 & 体验
+
+<appcenter>
+
+- :fire: 通过 [Serverless 应用中心](https://fcnext.console.aliyun.com/applications/create?template=FCTablestoreSinkConnector) ，
+[![Deploy with Severless Devs](https://img.alicdn.com/imgextra/i1/O1CN01w5RFbX1v45s8TIXPz_!!6000000006118-55-tps-95-28.svg)](https://fcnext.console.aliyun.com/applications/create?template=FCTablestoreSinkConnector)  该应用。 
+
+</appcenter>
+
+- 通过 [Serverless Devs Cli](https://www.serverless-devs.com/serverless-devs/install) 进行部署：
+    - [安装 Serverless Devs Cli 开发者工具](https://www.serverless-devs.com/serverless-devs/install) ，并进行[授权信息配置](https://www.serverless-devs.com/fc/config) ；
+    - 初始化项目：`s init FcTableStoreSinkConnector -d FcTableStoreSinkConnector`   
+    - 进入项目，并进行项目部署：`cd FcTableStoreSinkConnector && s deploy -y`
+
+</deploy>
+
+<appdetail id="flushContent">
+
+## 使用步骤
+
+### 资源准备
+
+依次创建如下资源：
+- tablestore 实例
+- tablestore 数据表，按需创建主键
+
+### 应用部署
+
+您可以通过应用中心或直接使用 s 工具进行部署。部署之前，需要对函数的环境变量进行如下检查：
+- endpoint 以及 instanceName：是否与目标 tablestore 实例名称匹配
+- tableName：是否与目标 tablestore 数据表名称匹配
+- primaryKeysName：是否与目标 tablestore 数据表主键匹配
+
+### 参数介绍
+
+应用部署完成后，可以构造函数请求参数来对函数发起调用，测试函数的功能正确性。`eventSchema` 为 cloudEvent 且 `batchOrNot` 为 false 时的调用参数[示例](src/event-template/sink-single-event.json)可以参考：
+
+```
+{
+"data":{
+    "primaryKey1":"xx",
+    "primaryKey2":"xx",
+    "colName1":"xx",
+    "colName2":"xx"
+},
+"id":"xx",
+"source":"acs:mns",
+"specversion":"1.0",
+"type":"mns:Queue:SendMessage",
+"datacontenttype":"application/json; charset\\u003dutf-8",
+"time":"xx-xx-xxT00:00:00.000Z",
+"subject":"acs:mns:cn-hangzhou:xxxx:queues/xxx",
+"aliyunaccountid":"xx"
+}
+```
+`eventSchema` 为 cloudEvent 且 `batchOrNot` 为 true 时，可将批量事件作为函数调用参数，其[示例]((./src/event-template/sink-batch-event.json))可以参考：
+```
+[
+{
+"data":{
+    "primaryKey1":"xx",
+    "primaryKey2":"xx",
+    "colName1":"xx",
+    "colName2":"xx"
+},
+"id":"xx",
+"source":"acs:mns",
+"specversion":"1.0",
+"type":"mns:Queue:SendMessage",
+"datacontenttype":"application/json; charset\\u003dutf-8",
+"time":"xx-xx-xxT00:00:00.000Z",
+"subject":"acs:mns:cn-hangzhou:xxxx:queues/xxx",
+"aliyunaccountid":"xx"
+},
+{
+"data":{
+    "primaryKey1":"xx",
+    "primaryKey2":"xx",
+    "colName1":"xx",
+    "colName2":"xx"
+},
+"id":"xx",
+"source":"acs:mns",
+"specversion":"1.0",
+"type":"mns:Queue:SendMessage",
+"datacontenttype":"application/json; charset\\u003dutf-8",
+"time":"xx-xx-xxT00:00:00.000Z",
+"subject":"acs:mns:cn-hangzhou:xxxx:queues/xxx",
+"aliyunaccountid":"xx"
+}
+]
+```
+
+上述参数中的 `data` 中的内容最终会被写入到 tablestore 数据表中。 
+
+### 应用调用
+参数构造完成后，可以通过控制台或者 s 工具进行调用，本文以调用 sink function 为例，对这两种方式进行介绍。
+
+#### 控制台调用
+
+应用部署完成后，按照如下步骤进行在控制台进行函数调用：
+1. 进入[函数计算控制台](https://fcnext.console.aliyun.com/cn-hangzhou/services)，找到对应的 sink 函数
+2. 进入测试函数页面，将[参数介绍](#参数介绍)中的参数输入事件输入框中，点击测试函数即可。
+![函数测试](https://img.alicdn.com/imgextra/i2/O1CN01VWhwGH1Rx70AiHRfr_!!6000000002177-2-tps-3576-1908.png)
+
+#### s 工具调用
+应用部署完成后，按照如下步骤进行通过 s 工具进行函数调用：
+1. 进入应用项目工程下，在 s.yml 查找应用初始化时输入的 `batchOrNot` 值，若：
+   - `batchOrNot` 为 False：将文件 event-template/sink-single-event.json 中的值修改为目标值后，执行 `s sink invoke --event-file event-template/sink-single-event.json` 进行函数调用
+   - `batchOrNot` 为 True：将文件 event-template/sink-batch-event.json 中的值修改为目标值后，执行 `s sink invoke --event-file event-template/sink-batch-event.json` 进行函数调用
+2. 函数调用完成后，可以登陆到 [tablestore 控制台](https://otsnext.console.aliyun.com/)查看目标数据表中是否已经写入数据。
+
+### 高级功能
+
+#### 通过不同网络访问 tablestore 实例
+有如下三种网络访问方式：
+- 经典网络访问：通过阿里云内网进行访问，不会产生公网流量，同时安全性也高于公网访问,推荐使用
+- 公网访问：函数环境变量中的 endpoint 为公网访问地址时，需要 tablestore 开启“公网访问”能力，同时会产生公网流量
+- vpc 访问：通过 vpc 网络进行访问，不会产生公网流量，需要为 sink 函数配置 vpc
+
+#### 自定义数据转储处理逻辑
+本应用创建后会生成两个函数：transform 函数及 sink 函数。如果您需要对原始数据进行处理，可以编写 transform 函数中的 transform 方法，以便按照您需要的方式对数据进行预处理。
+
+#### 修改数据库存入点及存入方式
+本应用默认将原始数据每条生成一个记录，并将整体 json decode 后存入 data 表中。如果您需要自定义转储方式，可以对 sink 函数 sink() 方法中的 tablestore 语句进行修改。
+
+
+
+</appdetail>
+
+<devgroup>
+
+## 开发者社区
+
+您如果有关于错误的反馈或者未来的期待，您可以在 [Serverless Devs repo Issues](https://github.com/serverless-devs/serverless-devs/issues) 中进行反馈和交流。如果您想要加入我们的讨论组或者了解 FC 组件的最新动态，您可以通过以下渠道进行：
+
+<p align="center">
+
+| <img src="https://serverless-article-picture.oss-cn-hangzhou.aliyuncs.com/1635407298906_20211028074819117230.png" width="130px" > | <img src="https://serverless-article-picture.oss-cn-hangzhou.aliyuncs.com/1635407044136_20211028074404326599.png" width="130px" > | <img src="https://serverless-article-picture.oss-cn-hangzhou.aliyuncs.com/1635407252200_20211028074732517533.png" width="130px" > |
+|--- | --- | --- |
+| <center>微信公众号：`serverless`</center> | <center>微信小助手：`xiaojiangwh`</center> | <center>钉钉交流群：`33947367`</center> | 
+
+</p>
+
+</devgroup>
---
+++ serverless-tablestore-sink-connector/src/sink/index.py
@@ -0,0 +1,296 @@
+# -*- coding: utf-8 -*-
+import json
+import logging
+import os
+
+from schema import Schema
+from tablestore import BatchWriteRowRequest
+from tablestore import Condition
+from tablestore import OTSClient
+from tablestore import PutRowItem
+from tablestore import Row
+from tablestore import RowExistenceExpectation
+from tablestore import TableInBatchWriteRowItem
+from retrying import retry
+
+import sink_schema
+
+logger = logging.getLogger()
+default_retry_times = 3
+
+def result_need_retry(result):
+    """if the result needes to be retried.
+
+    Args:
+        result: bool, if the function call succeeded
+
+    Returns:
+        Bool, if result == True, return False otherwise return True
+
+    Raises:
+        None
+    """
+    if result:
+        return False
+    return True
+
+class Sink(object):
+    """Sink Class.
+
+     The main class deal with the incoming message and put to sink target.
+     """
+
+    def __init__(self):
+        """Class Initializer. Initialization should realized in connect method.
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+        self.connected = False
+
+
+    def connect(self, sink_config, access_key_id, access_key_secret, security_token):
+        """Sink connector construct method.
+            todo: User should realize this method
+
+        Args:
+            sink_config: config of this sink connector
+            access_key_id: access key id
+            access_key_secret: access key secret
+            security_token: security token
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+        self.connected = True
+        primary_keys_name = sink_config['primaryKeysName']
+        rows_name = sink_config['colsName']
+        self.sink_config = sink_config
+        self.sink_config['primaryKeysName'] = primary_keys_name.split(',')
+        self.sink_config['colsName'] = rows_name.split(',')
+
+        try:
+            try:
+                security_token
+            except NameError:
+                # security_token is undefined
+                self.client = OTSClient(end_point=self.sink_config['endpoint'],
+                                        access_key_id=access_key_id,
+                                        access_key_secret=access_key_secret,
+                                        instance_name=self.sink_config['instanceName'],
+                                        socket_timeout=20)
+            else:
+                self.client = OTSClient(end_point=self.sink_config['endpoint'],
+                                        access_key_id=access_key_id,
+                                        access_key_secret=access_key_secret,
+                                        instance_name=self.sink_config['instanceName'],
+                                        sts_token=security_token, socket_timeout=20)
+
+        except Exception as e:
+            logger.error(e)
+            logger.error(
+                "ERROR: Unexpected error: Could not connect to Tablestore instance.")
+            raise Exception(str(e))
+        pass
+
+
+    def close(self):
+        """Sink connector deconstruct method.
+            todo: User should realize this method
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+        self.connected = False
+        pass
+
+
+    def is_connected(self):
+        """Sink connector connect check.
+
+         Args:
+             None
+
+         Returns:
+             None
+
+         Raises:
+             None
+         """
+        return self.connected
+
+    @retry(stop_max_attempt_number=default_retry_times, wait_exponential_multiplier=1000, retry_on_result=result_need_retry)
+    def deliver(self, payload):
+        """Sink operator.
+            deliver data to ots
+
+        Args:
+            payload: input payload
+
+        Returns:
+            success flag
+
+        Raises:
+            Exception
+        """
+        try:
+            data_list = []
+            if self.sink_config['batchOrNot'] == "True":
+                for single_payload in payload:
+                    data_list.append(single_payload['data'])
+            else:
+                data_list.append(payload['data'])
+
+            put_row_items = self._get_row(data_list)
+
+            # batch write data list into table
+            table_name = self.sink_config['tableName']
+            req = BatchWriteRowRequest()
+            req.add(TableInBatchWriteRowItem(table_name, put_row_items))
+            resp = self.client.batch_write_row(req)
+            succeed_items, failed_items = resp.get_put()
+
+            for item in failed_items:
+                logger.error('Put item failed, error code: %s, error message: %s' % (item.error_code, item.error_message))
+            return resp.is_all_succeed()
+        except Exception as e:
+            logger.error('deliver failed due to exception: ' + str(e))
+            return false
+
+    def _get_row(self, data_list):
+        """Inner method to get tablestore put row.
+
+        Args:
+            data_list: data list
+
+        Returns:
+            put_row_items: tablestore put row items
+        Raises:
+            None
+        """
+        put_row_items = []
+        for data in data_list:
+            pks = []
+            cols = []
+            primary_keys_name = self.sink_config['primaryKeysName']
+            rows_name = self.sink_config['colsName']
+            for key in primary_keys_name:
+                logger.info("primary key: %s, value %s", key, data[key])
+                pks.append((key, data[key]))
+            for row in rows_name:
+                logger.info("row name: %s, value: %s", row, data[row])
+                cols.append((row, data[row]))
+
+            row = Row(pks, cols)
+            condition = Condition(RowExistenceExpectation.IGNORE)
+            item = PutRowItem(row, condition)
+            put_row_items.append(item)
+        return put_row_items
+
+
+sink = Sink()
+
+
+def initialize(context):
+    """Sink function initializer.
+        this method is called before the function invocation,
+        and will be only called once in a specified container.
+        todo: User should realize this method
+
+    Args:
+        context: fc function invocation context
+
+    Returns:
+        None
+
+    Raises:
+        todo: xx
+    """
+    logger.info('initializing sink connect')
+    creds = context.credentials
+    sink_config_env = os.environ.get('SINK_CONFIG')
+    sink_config = json.loads(sink_config_env)
+    if not sink_schema.validate_sink_config_schema(sink_config):
+        logger.error("validate failed error: %s",
+                     Schema(sink_schema.SINK_CONFIG_SCHEMA, ignore_extra_keys=True).validate(sink_config))
+        raise Exception("SINK_CONFIG_SCHEMA validate failed")
+
+
+    sink.connect(sink_config, creds.access_key_id, creds.access_key_secret, creds.security_token)
+
+
+def destroy(context):
+    """Sink function deconstructor.
+       This method is called as pre-stop, which will be executed before the function container releasement.
+        todo: User should realize this method
+
+    Args:
+        context: fc function invocation context
+
+    Returns:
+        None
+
+    Raises:
+        todo: xx
+    """
+    sink.close()
+
+
+def handler(event, context):
+    """FC Function handler.
+
+     Args:
+         event: FC function invocation payload.
+
+     Returns:
+         context:  FC function invocation context. Valid params see: https://help.aliyun.com/document_detail/422182.html.
+
+     Raises:
+         Exception.
+     """
+    try:
+
+        payload = json.loads(event)
+
+        # only single data type is validated here.
+        if (sink.sink_config['batchOrNot'] == "False") and (sink.sink_config["eventSchema"] == "cloudEvent"):
+            logger.info("check single data with schema: cloudEvent")
+            if not sink_schema.validate_message_schema(payload):
+                logger.error("validate failed error: %s",
+                             Schema(sink_schema.MESSAGE_SCHEMA, ignore_extra_keys=True).validate(payload))
+                raise Exception("MESSAGE_SCHEMA validate failed")
+
+        if (sink.sink_config['batchOrNot'] == "True") and (sink.sink_config["eventSchema"] == "cloudEvent"):
+            logger.info("check batch data with schema: cloudEvent")
+            for single_payload in payload:
+                if not sink_schema.validate_message_schema(single_payload):
+                    logger.error("validate failed error: %s",
+                                 Schema(sink_schema.MESSAGE_SCHEMA, ignore_extra_keys=True).validate(single_payload))
+                    raise Exception("MESSAGE_SCHEMA validate failed")
+
+        if not sink.is_connected():
+            raise Exception("unconnected sink target")
+
+        is_succ = sink.deliver(payload)
+        if is_succ is False:
+            return json.dumps({"success": False, "error_message": "sink to ots failed."})
+
+    except Exception as e:
+        logger.error(e)
+        return json.dumps({"success": False, "error_message": str(e)})
+
+    return json.dumps({"success": True, "error_message": ""})
